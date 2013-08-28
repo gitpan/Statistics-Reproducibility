ad23a63a67dbbf58e477ab93cfe4acee667baef2
Jimi Carlo Wills <jimi@cpan.org>
2013-08-28 18:14:49 +0000

    Import of JIMI/Statistics-Reproducibility-0.03 from CPAN.
    gitpan-cpan-distribution: Statistics-Reproducibility
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         JIMI/Statistics-Reproducibility-0.03.tar.gz
    gitpan-cpan-author:       JIMI
    gitpan-cpan-maturity:     released

--- lib/Statistics/Reproducibility.pm
+++ lib/Statistics/Reproducibility.pm
@@ -16,11 +16,11 @@ Statistics::Reproducibility - Reproducibility measurement between multiple repli
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
